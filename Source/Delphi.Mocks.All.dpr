0c7a80baa002c3dab2e6a323c7a0b45d2b9c2a26
Serhii M. Kheilyk <1171495+lynxnake@users.noreply.github.com>
2020-11-05 13:15:15 +0200

    Add fake library to compile all units at once

---
+++ Source/Delphi.Mocks.All.dpr
@@ -0,0 +1,26 @@
+library Delphi.Mocks.All;
+
+{ Not a real library, but just a mean to compile all Delphi.Mocks units from the IDE }
+
+uses
+  Delphi.Mocks.AutoMock in 'Delphi.Mocks.AutoMock.pas',
+  Delphi.Mocks.Behavior in 'Delphi.Mocks.Behavior.pas',
+  Delphi.Mocks.Expectation in 'Delphi.Mocks.Expectation.pas',
+  Delphi.Mocks.Helpers in 'Delphi.Mocks.Helpers.pas',
+  Delphi.Mocks.Interfaces in 'Delphi.Mocks.Interfaces.pas',
+  Delphi.Mocks.MethodData in 'Delphi.Mocks.MethodData.pas',
+  Delphi.Mocks.ObjectProxy in 'Delphi.Mocks.ObjectProxy.pas',
+  Delphi.Mocks.ParamMatcher in 'Delphi.Mocks.ParamMatcher.pas',
+  Delphi.Mocks in 'Delphi.Mocks.pas',
+  Delphi.Mocks.Proxy in 'Delphi.Mocks.Proxy.pas',
+  Delphi.Mocks.Proxy.TypeInfo in 'Delphi.Mocks.Proxy.TypeInfo.pas',
+  Delphi.Mocks.ReturnTypePatch in 'Delphi.Mocks.ReturnTypePatch.pas',
+  Delphi.Mocks.Utils in 'Delphi.Mocks.Utils.pas',
+  Delphi.Mocks.Validation in 'Delphi.Mocks.Validation.pas',
+  Delphi.Mocks.VirtualInterface in 'Delphi.Mocks.VirtualInterface.pas',
+  Delphi.Mocks.VirtualMethodInterceptor in 'Delphi.Mocks.VirtualMethodInterceptor.pas',
+  Delphi.Mocks.WeakReference in 'Delphi.Mocks.WeakReference.pas',
+  Delphi.Mocks.When in 'Delphi.Mocks.When.pas';
+
+begin
+end.
